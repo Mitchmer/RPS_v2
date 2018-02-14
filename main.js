1bb73d166634839e2d8c68fb719c3e13e91aced2
Mitch Merrell <mitchmerrell88@gmail.com>
2018-02-14 16:17:04 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+// $(document).ready( function() {
+
+
+  function userInput(user) {
+    var user = this.id
+    console.log(user)
+    compInput()
+  }
+
+  function compInput() {
+    
+  }
+
+//   function comparison() {
+
+//   }
+
+//   function result() {
+
+//   }
+// })
+
+$('.choice').on('click', userInput)
